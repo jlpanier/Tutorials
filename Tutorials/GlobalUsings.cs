98bdb40caf1796a43f4b5228ad2803031e2bbc33
Jean-Luc <jean-luc.panier@outlook.com>
2026-03-02 23:49:53 +0100

    Passage en net10.0

--- Tutorials/GlobalUsings.cs
+++ Tutorials/GlobalUsings.cs
@@ -1,6 +1,4 @@
 global using Fonts;
-global using Tutorials.Data;
-global using Tutorials.PageModels;
 global using Tutorials.Pages;
 global using Tutorials.Services;
 global using Tutorials.Utilities;
