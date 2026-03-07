d0f5f1d4cd35609241b077af3564f8055a5324c3
Jean-Luc <jean-luc.panier@outlook.com>
2026-03-07 22:43:18 +0100

    Animations

---
+++ Tutorials/Pages/Animations/FadePage.xaml.cs
@@ -0,0 +1,16 @@
+namespace Tutorials.Pages.Animations;
+
+public partial class FadePage : ContentPage
+{
+	public FadePage()
+	{
+		InitializeComponent();
+	}
+
+    private async void OnClickedAsync(object sender, EventArgs e)
+    {
+        await BotImg.FadeToAsync(0);
+        await Task.Delay(1000);
+        await BotImg.FadeToAsync(1);
+    }
+}
